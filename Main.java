94b3ab71d2cf0ee9b0704e4354839683e1a237e5
Akshay Gajanan Falfale <akshayfalfale@gmail.com>
2023-12-27 23:51:09 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is my first program");
+        System.out.println("This is the second line on second line");
+        System.out.println("Hello world is typing");
+        System.out.println("This is line 4");
+
+        int a= 10; int b=20;
+        int c= a+b;
+    }
+}
